679947b8eb59e80ca57dc263a836ce600a5be488
Cank01 <cankarsten@web.de>
2026-09-21 22:18:44 +0200

    created widgets for landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar1 } from "@/components/navbar1";
+import { Footer2 } from "@/components/footer2";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,9 +23,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar1 />
+        {children}
+        <Footer2 />
+      </body>
     </html>
   );
 }
